f7bf37a0ac91549a0ac8bc2dae07d983e0262e1b
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2020-04-16 01:01:19 -0400

    Update run_MLE.sh

--- siahkoohi2020SEGwdp/run_MLE.sh
+++ siahkoohi2020SEGwdp/run_MLE.sh
@@ -6,5 +6,5 @@ repo_name=Software.SEG2020/siahkoohi2020SEGwdp
 path_script=$HOME/$repo_name/src
 vel_dir=$HOME/$repo_name/vel_dir
 
-python main.py --epoch 411 --eta 0.01 --lr 0.001 --experiment $experiment_name \
+python $path_script/main.py --epoch 411 --eta 0.01 --lr 0.001 --experiment $experiment_name \
 	--save_freq 100 --sample_freq 20 --objective MLE_x --cuda 0 --vel_dir $vel_dir
